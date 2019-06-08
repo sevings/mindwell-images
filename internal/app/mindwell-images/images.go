1f132b1870d26af25c67b55366b9bc76ee472b92
binque <binque@ya.ru>
2019-06-08 10:16:23 +0300

    save image size

--- internal/app/mindwell-images/images.go
+++ internal/app/mindwell-images/images.go
@@ -11,6 +11,15 @@ import (
 	goconf "github.com/zpatrick/go-config"
 )
 
+func saveImageSize(tx *utils.AutoTx, imageID, width, height int64, size string) {
+	const q = `
+		INSERT INTO image_sizes(image_id, size, width, height)
+		VALUES($1, (SELECT id FROM size WHERE type = $2), $3, $4)
+	`
+
+	tx.Exec(q, imageID, size, width, height)
+}
+
 func NewImageUploader(db *sql.DB, cfg *goconf.Config) func(images.PostImagesParams, *models.UserID) middleware.Responder {
 	return func(params images.PostImagesParams, userID *models.UserID) middleware.Responder {
 		store := newImageStore(cfg)
@@ -36,6 +45,12 @@ func NewImageUploader(db *sql.DB, cfg *goconf.Config) func(images.PostImagesPara
 		return utils.Transact(db, func(tx *utils.AutoTx) middleware.Responder {
 			tx.Query("INSERT INTO images(user_id, path) VALUES($1, $2) RETURNING id", userID.ID, store.FileName())
 			tx.Scan(&img.ID)
+
+			saveImageSize(tx, img.ID, img.Thumbnail.Width, img.Thumbnail.Height, "thumbnail")
+			saveImageSize(tx, img.ID, img.Small.Width, img.Small.Height, "small")
+			saveImageSize(tx, img.ID, img.Medium.Width, img.Medium.Height, "medium")
+			saveImageSize(tx, img.ID, img.Large.Width, img.Large.Height, "large")
+
 			if tx.Error() != nil {
 				return images.NewPostImagesBadRequest()
 			}
